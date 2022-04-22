5b77ebdeff624cabf9a9ab8da574bfd4a531e64c
René M. Puig <99565865+ReneManu@users.noreply.github.com>
2022-04-22 09:13:49 -0400

    web update

---
+++ wp-content/themes/vantage/inc/contributors.php
@@ -0,0 +1,80 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 7165,
+		"score": 132.81968806067007,
+		"percent": 64.08858213953052
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 1480,
+		"score": 69.05887054721128,
+		"percent": 33.3225078461747
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 1605,
+		"score": 3.188067077995404,
+		"percent": 1.538316357896517
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 161,
+		"score": 0.9702709765269398,
+		"percent": 0.4681782654717941
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 588,
+		"score": 0.3928257984321357,
+		"percent": 0.18954756495019567
+	},
+	"be6a5d2323cfe9ec8246b7a677dbc65b": {
+		"name": "Aaron Evans",
+		"email": "be6a5d2323cfe9ec8246b7a677dbc65b",
+		"loc": 23,
+		"score": 0.2682432380854639,
+		"percent": 0.12943358811053035
+	},
+	"9e7a3b2d24c2b15c53209ba8e7b4e724": {
+		"name": "Kevin Batdorf",
+		"email": "9e7a3b2d24c2b15c53209ba8e7b4e724",
+		"loc": 22,
+		"score": 0.2407648738877063,
+		"percent": 0.1161746396318718
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 13,
+		"score": 0.1567793184948974,
+		"percent": 0.07564965990998396
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 19,
+		"score": 0.09778495775063693,
+		"percent": 0.04718351163382938
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 20,
+		"score": 0.0419518949394413,
+		"percent": 0.02024276298184956
+	},
+	"07294cc1dd7658895e44c559dfffd76f": {
+		"name": "gpriday",
+		"email": "07294cc1dd7658895e44c559dfffd76f",
+		"loc": 20,
+		"score": 0.008670388548525285,
+		"percent": 0.004183663708199528
+	}
+}', true );
